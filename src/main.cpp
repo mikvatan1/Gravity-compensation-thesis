e990d6d0543e1695756318b94ff60a395bdc3069
mikvatan1 <mikvatan@hotmail.com>
2025-08-21 15:27:13 +0200

    good

--- src/main.cpp
+++ src/main.cpp
@@ -50,7 +50,7 @@ uint8_t pendingR = 0, pendingG = 0, pendingB = 0; // Pending LED colors
 
 Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-PIDController pid(1.0, 0.7, 0.05); 
+PIDController pid(1.0, 0.7, 0.10); 
 
 // error_a_max = 100mm
 // therefore Kp*e = 100 (as other terms are not aggressive)
